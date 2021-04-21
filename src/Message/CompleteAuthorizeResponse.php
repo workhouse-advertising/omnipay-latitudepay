2f111e140df70895ef4c852046517f00ad3ae9d6
Simon Geoghegan <simon.geoghegan@workhouseadvertising.com.au>
2021-04-21 10:58:36 +0800

    Added message handling to the CompleteAuthorizeResponse

--- src/Message/CompleteAuthorizeResponse.php
+++ src/Message/CompleteAuthorizeResponse.php
@@ -8,32 +8,35 @@ class CompleteAuthorizeResponse extends Response
 {
     const RESULT_COMPLETE = 'COMPLETED';
 
+    /**
+     * @inheritDoc
+     */
     public function isSuccessful()
     {
         return ($this->getData()['result'] ?? null) === static::RESULT_COMPLETE;
     }
 
-    // TODO: See about adding message handling.
-
-    // TODO: Check available statuses and add checks here.
-
-    // public function isPending()
-    // {
-    //     return false;
-    // }
-
-    // public function isCancelled()
-    // {
-    //     return false;
-    // }
-
+    /**
+     * @inheritDoc
+     */
     public function getTransactionId()
     {
         return ($this->getData()['reference'] ?? null);
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getTransactionReference()
     {
         return ($this->getData()['token'] ?? null);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMessage()
+    {
+        return $this->getData()['message'] ?? null;
     }
 }
